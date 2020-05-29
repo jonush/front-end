4e11f76cc3542b8632955e6d0eb086bf613d3ecd
Jonathan Hsu <jonhsu.design@gmail.com>
2020-05-29 11:03:42 -0700

    minor styling update

--- how-to/src/components/Navbar.jsx
+++ how-to/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../HowTo.png';
-import About from '../components/About';
 import '../App.css';
 
 const Navbar = ({ loggedIn, setLoggedIn }) => {
@@ -21,8 +20,11 @@ const Navbar = ({ loggedIn, setLoggedIn }) => {
       <NavLink to='/about'>About</NavLink>
         {
           loggedIn === true ? 
-          <NavLink to='/login' onClick={logOut}>Logout</NavLink> :
-          <NavLink to='/login'>Login</NavLink>
+          <>
+            <NavLink to='/dashboard'>Dashboard</NavLink>
+            <NavLink to='/login' onClick={logOut}>Logout</NavLink>
+          </> :
+            <NavLink to='/login'>Login</NavLink>
         }
       </div>
     </nav>
